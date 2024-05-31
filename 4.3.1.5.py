7dc8e4effebb0aae4edeb3247284b1417e63c40a
Nagalie <151873220+Nagalie@users.noreply.github.com>
2024-05-31 17:30:05 +0200

    hh

--- 4.3.1.5.py
+++ 4.3.1.5.py
@@ -2,3 +2,7 @@ Preis = int(input("Bitte geben Sie den Einkaufspreis der Maschine ein: "))
 Jahre = int(input("Bitte geben Sie die Jahre ein die linear abgeschrieben werden."))
 
 Zahlung = Preis / Jahre
+
+for jahr in range(1, Jahre + 1):
+    restwert = Preis - (Zahlung * jahr)
+    print("Jahr " +jahr + ": " + restwert)
---
+++ 4.3.1.6.py
@@ -0,0 +1,6 @@
+Kapital = int(input("Fester Jahresbetrag: "))
+Zinssatz = int(input("Gebe dein Zins ein: "))
+Wunschbetrag = int(input("Gebe deinen Betrag ein den du erreichen willst: "))
+
+for Jahr in range(0, Kapital == Wunschbetrag):
+    
